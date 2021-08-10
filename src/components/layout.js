5ceb4614c61331b31215e4018b4ab61cb167fb91
Monilito Castro <mannycastro.epic@gmail.com>
2021-08-10 11:11:50 -0400

    created mdx files

---
+++ src/components/layout.js
@@ -0,0 +1,41 @@
+// Step 1: Import your component
+import * as React from 'react'
+import {Link, useStaticQuery, graphql} from "gatsby"
+import { 
+    container,
+    heading,
+    navLinks,
+    navLinkItem,
+    navLinkText } from './layout.module.css'
+
+const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`)
+    return (
+      <div className={container}>
+        <title>{data.site.siteMetadata.title} - {pageTitle}</title>
+        <main>
+        <nav>
+          <h1>{data.site.siteMetadata.title}</h1>
+            <ul className={navLinks}>
+                <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
+                <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
+            </ul>
+        </nav>
+          <h3 className={heading}>{pageTitle || data.site.siteMetadata.title}</h3>
+        <p>{data.site.siteMetadata.description}</p>
+          {children}
+        </main>
+      </div>
+    )
+  }
+
+// Step 3: Export your component
+export default Layout
